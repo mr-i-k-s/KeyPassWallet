78826deda37cd3efc78d292b18f70a3776277967
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-06 07:04:23 +0800

    Add class KeyPassData with props

---
+++ KeyPassWallet/MVVM/Models/KeyPassData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyPassWallet.MVVM.Models
+{
+	public class KeyPassData
+	{
+        public string Title { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Url { get; set; }
+        public string Notes { get; set; }
+    }
+}
