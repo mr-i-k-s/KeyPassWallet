5111bb8cac36b90a55c5e1a656bc6efba433123e
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-06 07:15:33 +0800

    Add class LoginViewModel

---
+++ KeyPassWallet/MVVM/ViewModels/LoginViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.MVVM.ViewModels;
+
+namespace KeyPassWallet.MVVM.ViewModels
+{
+	public class LoginViewModel : ViewModelBase
+	{
+        #region Variables
+
+        #endregion Variables
+
+        #region Properties
+
+        #endregion Properties
+
+        #region Constructors
+
+        public LoginViewModel()
+        {
+            
+        }
+
+        #endregion Constructors
+
+        #region Methodes
+
+        #endregion Methodes 
+
+        #region Observable Property Methodes
+
+        #endregion Observable Property Methodes
+
+        #region RelayCommands
+
+        #endregion RelayCommands
+
+        #region Messages
+
+        #endregion Messages
+
+        #region Events
+
+        #endregion Events
+
+        #region Dispose
+
+        #endregion Dispose
+    }
+}
