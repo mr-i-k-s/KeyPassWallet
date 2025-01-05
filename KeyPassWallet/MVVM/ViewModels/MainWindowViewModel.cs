4b55198353db070a2a22cf04a2179163267b997b
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-06 07:13:32 +0800

    add prop to class MainWindowViewModel

--- KeyPassWallet/MVVM/ViewModels/MainWindowViewModel.cs
+++ KeyPassWallet/MVVM/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,48 @@ namespace KeyPassWallet.MVVM.ViewModels
 {
 	public class MainWindowViewModel : ViewModelBase
 	{
-	}
+        #region Variables
+
+        [ObservableProperty]
+        private ViewModelBase currViewModel;
+
+        #endregion Variables
+
+        #region Properties
+
+        #endregion Properties
+
+        #region Constructors
+
+        #endregion Constructors
+
+        #region Methodes
+
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        #endregion Methodes 
+
+        #region Observable Property Methodes
+
+        #endregion Observable Property Methodes
+
+        #region RelayCommands
+
+        #endregion RelayCommands
+
+        #region Messages
+
+        #endregion Messages
+
+        #region Events
+
+        #endregion Events
+
+        #region Dispose
+
+        #endregion Dispose
+    }
 }
