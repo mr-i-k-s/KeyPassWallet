ba8cc0bf01ff729de4feba8ded8c33a28158b90e
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-06 08:32:41 +0800

    Add class WalletExplorerViewModel

---
+++ KeyPassWallet/MVVM/ViewModels/WalletExplorerViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.MVVM.ViewModels;
+
+namespace KeyPassWallet.MVVM.ViewModels
+{
+	public partial class WalletExplorerViewModel : ViewModelBase
+	{
+        #region Variables
+
+        #endregion Variables
+
+        #region Properties
+
+        #endregion Properties
+
+        #region Constructors
+
+        public WalletExplorerViewModel()
+        {
+            
+        }
+
+        #endregion Constructors
+
+        #region Methodes
+
+        #endregion Methodes 
+
+        #region Observable Property Methodes
+
+        #endregion Observable Property Methodes
+
+        #region RelayCommands
+
+        #endregion RelayCommands
+
+        #region Messages
+
+        #endregion Messages
+
+        #region Events
+
+        #endregion Events
+
+        #region Dispose
+
+        #endregion Dispose
+    }
+}
