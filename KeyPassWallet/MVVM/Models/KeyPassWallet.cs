69f8d43b4baa4e2a31c8f357d86c626cde3afae2
DESKTOP-D9JTNBQ\IKS <IKS>
2025-01-06 08:36:08 +0800

    Add class KeyPassWallet

---
+++ KeyPassWallet/MVVM/Models/KeyPassWallet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyPassWallet.MVVM.Models
+{
+	public class KeyPassWallet
+	{
+        public string Title { get; set; }
+        public List<KeyPassData> Keys { get; set; }
+    }
+}
